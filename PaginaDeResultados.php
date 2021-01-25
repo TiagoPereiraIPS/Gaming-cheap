f5e906d65d405f987c373588a7f4cfe39471ddd2
tiagoquintino170100236 <170100236@esg.ipsantarem.pt>
2021-01-25 17:14:18 +0000

    Website V3
    ImagensJogos - Criação de uma pasta para adicionar capas dos jogos.
    PaginaDeResultados.php e css - Inserir dados de pesquisa na base de dados e inserir imagem de jogo no container e atualização de um campo no css.
    PaginaProcurar.html para PaginaProcurar.php - Criação de uma listagem na página dos jogos já pesquisados, acedendo à base de dados. Inserção de uma linha de código para não aparecer avisos se a base de dados estiver vazia.
    PaginaProcurar.css - Remoção de código desnecessário.

--- PaginaDeResultados.php
+++ PaginaDeResultados.php
@@ -1,11 +1,41 @@
 <?php
 
 require_once "GamingCheap.php";
+require_once "GamesMemory.php";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST['jogo'];
     $bot = new GamingCheap($name);
     $dados = $bot->retrieveData();
+}
+
+$db = new GamesMemory(); //Classe para controlar a BD, precisas de mysql server a correr com um user (ver credenciais no file SaveInfo.php)
+$db->install(); //verifica se tem as tabela criada, se não, cria uma
+
+foreach ($dados as $jogo){
+    //var_dump($jogo->mCouponText);
+	//Verificar se o registo já existe
+    if($db->alreadyThere($jogo->mGame, $jogo->mOnlineStore, $jogo->mPlatform, $jogo->mGameVersion)){
+        //Se já existe dá update
+		$db->update(
+        $jogo->mGame,
+        $jogo->mOnlineStore,
+        $jogo->mPlatform,
+        $jogo->mGameVersion,
+        $jogo->mCouponPercentageAndName,
+        $jogo->mPriceWithoutCoupon,
+        $jogo->mActualPrice);
+    } else {
+		//Se não existe faz um novo registo
+        $db->insertGames(
+        $jogo->mGame,
+        $jogo->mOnlineStore,
+        $jogo->mPlatform,
+        $jogo->mGameVersion,
+        $jogo->mCouponPercentageAndName,
+        $jogo->mPriceWithoutCoupon,
+        $jogo->mActualPrice);
+    }
 }
 
 ?>
@@ -53,7 +83,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <header>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <div class="container-fluid">
-                <a class="navbar-brand" href="PaginaProcurar.html">
+                <a class="navbar-brand" href="PaginaProcurar.php">
                     <img src="Gaming Cheap.png" alt="" width="150" height="100px" class="d-inline-block align-top">
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -89,8 +119,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     <?php foreach ($dados as $registo) { ?>
                         <div class="col">
                             <div class="card shadow-sm">
-                                
-
                                 <div class="card-body">
                                     <p class="card-text"><span style="font-weight:bold;"><?php echo $registo->{'mGame'} ?></span></p>
                                     <p class="card-text"><span style="font-weight:bold;">Loja online: </span><?php echo $registo->{'mOnlineStore'} ?></p>
@@ -103,6 +131,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                                         <p class="card-text"><span style="font-weight:bold;">Preço sem cupão: </span><?php echo $registo->{'mPriceWithoutCoupon'} ?></p>
                                     <?php  }  ?>
                                     <p class="card-text"><span style="font-weight:bold;">Preço final: </span><?php echo $registo->{'mActualPrice'} ?></p>
+                                    <img id="ImagemJogo" class="rounded float-right "  style="position: absolute; right: 0%; top: 0%; " src="ImagensJogos/<?php echo $registo->{'mGame'} ?>.jpg">
                                 </div>
                             </div>
                         </div>
